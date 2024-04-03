34e93039db8c130e78d0f9506451b9b75b761064
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-04-03 11:35:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "amount_of_cents,expected_result",
+    [
+        pytest.param(1, [1, 0, 0, 0], id="1 penny"),
+        pytest.param(6, [1, 1, 0, 0], id="1 penny + 1 nickel"),
+        pytest.param(17, [2, 1, 1, 0], id="2 pennies + 1 nickel + 1 dime"),
+        pytest.param(50, [0, 0, 0, 2], id="2 quarters")
+    ]
+)
+def test_smallest_possible_number_of_coins(amount_of_cents: int,
+                                           expected_result: list) -> None:
+    assert get_coin_combination(amount_of_cents) == expected_result
